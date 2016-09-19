e68a68ae003af882164742e143414b7f6313c225
grantmor <grant.morris.dev@gmail.com>
2016-09-19 13:28:52 -0400

    General cleanup
    - Added comments
    - Reduced code width to <= 80 characters
    - Moved initialization code into initializePage() function
    - Moved functions into a more sensible order

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 var studentsPerPage = 10;
-var $searchBox = $('<div class="student-search"><input placeholder="Search for students..."><button>Search</button></div>');
+var $searchBox = $('<div class="student-search"> \
+                    <input placeholder="Search for students..."> \
+                    <button>Search</button></div>');
 
 function hideAllStudents() {
   $('.student-item').hide();
@@ -24,12 +26,6 @@ function generatePageNav(numPerPage) {
   return pageNav += '</ul></div>';
 }
 
-function updatePageNav() {
-  clearPageNav();
-  $('.page').append(generatePageNav(studentsPerPage));
-  $('.pagination ul li a').on('click', selectPage);
-  selectPage(1);
-}
 // sets selected button to active and updates results
 // page argument is optional; this function is either a click handlers
 // or it can be manually invoked to display a given page
@@ -43,8 +39,8 @@ function selectPage(page) {
   // if called as an event handler
   if(typeof page === 'object') {
     pageNumber = $(this).text();
+  // if called with a page argument
   } else {
-    // NOT WORKING
     $buttons.first().addClass('active');
     pageNumber = page;
   }
@@ -65,18 +61,31 @@ function selectPage(page) {
   });
 }
 
+function updatePageNav() {
+  clearPageNav();
+  $('.page').append(generatePageNav(studentsPerPage));
+  $('.pagination ul li a').on('click', selectPage);
+  selectPage(1);
+}
+
 function selectAllStudents() {
   $('.student-item').toggleClass('selected');
 }
 
 function searchStudents() {
   var $searchField = $(this).siblings('input');
-  var text = $searchField.val().toLowerCase();
-  var searchExp = new RegExp(text);
-  var searchString;
   var $allStudents = $('.student-item');
   var $studentDetails = $('.student-item .student-details');
 
+  var text = $searchField.val().toLowerCase();
+  var searchExp = new RegExp(text);
+  var searchString;
+
+  // filter function - checks if search string finds a match in name or email
+  function searchForString() {
+      return $(this).find('h3').text().match(searchExp) !== null ||
+             $(this).find('.email').text().match(searchExp) !== null;
+  }
 
   if (text !== '') {
     $allStudents.removeClass('selected');
@@ -84,33 +93,38 @@ function searchStudents() {
     $allStudents.addClass('selected');
   }
 
+  // clear search field
   $searchField.val('');
 
   // search students
-  $studentDetails.filter(function () {
-    return $(this).find('h3').text().match(searchExp) !== null ||
-           $(this).find('.email').text().match(searchExp) !== null;
-  }).parent().addClass('selected');
+  $studentDetails.filter(searchForString).parent().addClass('selected');
+
+  // show only students selected by search
   hideAllStudents();
   $('.student-item.selected').show();
   updatePageNav();
 }
 
-// select all by default
-selectAllStudents();
-// hide all by default
-hideAllStudents();
-// inserting search box to the DOM
-$('.page-header').append($searchBox);
-// inserting page navigation into the DOM
-$('.page').append(generatePageNav(studentsPerPage));
-// setting active page to first
-$('.pagination ul li a:first').toggleClass('active');
-// display contents of first page (HACK - figure out how to reuse this with selectPage) // ? function that looks at current page and displays those ?
-$('.student-item').each(function (student) {
-  if (student < studentsPerPage)
-    $(this).show();
-});
+function initializePage() {
+  function displayFirstPage (student) {
+    if (student < studentsPerPage)
+      $(this).show();
+  }
+  // select all by default
+  selectAllStudents();
+  // hide all by default
+  hideAllStudents();
+  // inserting search box to the DOM
+  $('.page-header').append($searchBox);
+  // inserting page navigation into the DOM
+  $('.page').append(generatePageNav(studentsPerPage));
+  // setting active page to first
+  $('.pagination ul li a:first').toggleClass('active');
+  // display contents of first page
+  $('.student-item').each(displayFirstPage);
+}
+
+initializePage();
 
 /*** Event Handlers ***/
 // add click handlers to buttons
